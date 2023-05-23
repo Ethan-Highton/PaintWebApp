97af061070e5b4018fae95f8c84a37da784e373d
ethan-highton <ethan.highton247@gmail.com>
2023-05-22 21:05:55 -0400

    tenth commit

--- src/Menu/multiroom/multiroom.component.ts
+++ src/Menu/multiroom/multiroom.component.ts
@@ -1,32 +1,29 @@
-import { AfterViewInit, Component, ComponentFactoryResolver, ViewContainerRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 import { RectangleComponent } from '../rectangle/rectangle.component';
 
 @Component({
   selector: 'app-multiroom',
   templateUrl: './multiroom.component.html',
   styleUrls: ['./multiroom.component.css']
-  
 })
 export class MultiroomComponent implements AfterViewInit {
-  numbersArray: number[] = Array.from({ length: 9 }, (_, i) => i + 2)
+  numbersArray: number[] = Array.from({ length: 9 }, (_, i) => i + 2);
   isDropdownVisible = false;
-  roomIndexes: number[] = [];
-  rooms: RectangleComponent[] = []; 
   numberOfRooms!: number;
-  totalWallSpace: number = 0;
+  rooms: RectangleComponent[] = [];
   currentRoomIndex: number = 0;
   activeRoomIndex: number = 0;
+  totalWallSpace: number = 0;
 
-  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
-  
+  constructor(private resolver: ComponentFactoryResolver, private viewContainerRef: ViewContainerRef) {}
+
   ngAfterViewInit() {
     const menuLi = document.querySelector("#menuElem > li:nth-child(1)");
     // Perform any modifications to the menuLi element here
-    
   }
+
   showDropdown(event: MouseEvent) {
     this.isDropdownVisible = true;
-
   }
 
   hideDropdown(event: MouseEvent) {
@@ -37,15 +34,14 @@ export class MultiroomComponent implements AfterViewInit {
     this.numberOfRooms = value;
     this.currentRoomIndex = 0;
     this.activeRoomIndex = 0;
-    this.rooms = [];
     this.createRooms();
   }
 
-  constructor(private resolver: ComponentFactoryResolver, private viewContainerRef: ViewContainerRef) {}
-  
   createRooms() {
     this.rooms = [];
-  
+    this.currentRoomIndex = 0;
+    this.viewContainerRef.clear();
+
     for (let i = 0; i < this.numberOfRooms; i++) {
       const factory = this.resolver.resolveComponentFactory(RectangleComponent);
       const componentRef = this.viewContainerRef.createComponent(factory);
@@ -53,13 +49,23 @@ export class MultiroomComponent implements AfterViewInit {
       this.rooms.push(room);
     }
   }
+
   nextRoom() {
     if (this.currentRoomIndex < this.numberOfRooms) {
-      this.currentRoomIndex++;
-      this.activeRoomIndex++;
+      const currentRoom = this.rooms[this.currentRoomIndex];
+      const area = currentRoom.area;
+
+      if (area) {
+        this.currentRoomIndex++;
+        this.activeRoomIndex++;
+      } else {
+        alert("Please enter the area for the current room.");
+      }
     }
   }
-  onAreaCalculated(area: number) {
+
+  onAreaCalculated(event: Event) {
+    const area = +(event.target as HTMLInputElement).value;
     this.rooms[this.currentRoomIndex - 1].area = area;
   }
 
@@ -71,7 +77,3 @@ export class MultiroomComponent implements AfterViewInit {
     return this.totalWallSpace;
   }
 }
-
-
-
-
