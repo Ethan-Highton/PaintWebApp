2718c720f60dbe88facbf3302ec7efb0e91978e6
Ethan-Highton <ethan.highton247@gmail.com>
2025-03-21 01:40:46 -0400

    Eighteenth Commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'paintwebapp',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'paintwebapp',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
